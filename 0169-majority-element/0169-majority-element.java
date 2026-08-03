ce479df9fefe045e55ddcc6b271261f94b160486
JENNY KRUPAL MYLABATHULA <23a91a05b4@aec.edu.in>
2026-08-03 20:27:14 +0530

    Time: 6 ms (51.93%), Space: 56 MB (6.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,12 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         int freq = 0;
-        int ans = 0;
-        for(int i=0;i<nums.length;i++){
-            if(freq==0) ans=nums[i];
-            if(ans==nums[i]) freq++;
-            else freq--;
-        }
-        return ans;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
